8309f8aa555c44b25b3eb607ffd20cf390182cb6
Tom Klinovsky <tom.klino@gmail.com>
2018-11-13 16:28:54 +0200

    completed implementation of pairing by third party device and messages among linked peers

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,35 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model='message' />
+    <input type="button" v-on:click='sendMessage' value="send message" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+/* eslint-disable */
+import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'app',
+  methods: {
+    async sendMessage() {
+      console.log("sending the message:", this.message)
+      await this.socket.send(this.message)
+      this.message = '';
+    }
+  },
+  mounted() {
+    this.socket = new WebSocket('ws://localhost:8888');
+    this.socket.onmessage = (data) => {
+      console.log('got a message:', data)
+    }
+  },
+  data() {
+    return {
+      socket: null,
+      message: ''
+    }
+  },
   components: {
     HelloWorld
   }
