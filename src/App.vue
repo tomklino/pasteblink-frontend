87e99076a83ba83725183fe99aad0e21c7c35256
Tom Klinovsky <tom.klino@gmail.com>
2018-11-13 21:56:39 +0200

    copy on click button added, some (very) basic design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
+    <div id="notifications">
+      {{ last_notification }}
+    </div>
     <div id="server_message">
       <h1 v-if="!session.linked">Welcome! your client_id: {{ my_client_id }}</h1>
       <h1 v-if="session.linked && !session.active">In session, now scan the other device</h1>
       <h1 v-if="session.active">In active session!</h1>
     </div>
+    <div id="clips" v-if="session.active">
+      <div class="message" v-for="(message, index) in messages" :key="index">
+        <div class="message_text">{{message}}</div>
+        <button type="button"
+              v-clipboard:copy="message"
+              v-clipboard:success="onCopy"
+              v-clipboard:error="onCopyError">Copy!</button>
+      </div>
+    </div>
     <canvas v-if="!session.linked" id="canvas" ref="qrcanvas"></canvas>
     <div id="session_view" v-if="session.linked">
-      <input type="text" v-model='message' v-bind:disabled="!session.active"/>
-      <input type="button" v-on:click='sendMessage' value="send message" v-bind:disabled="!session.active"/>
+      <form v-on:submit.prevent="sendMessage">
+        <textarea rows="20" cols="100" v-model='message' v-bind:disabled="!session.active"/>
+        <input type="submit" value="send message" v-bind:disabled="!session.active"/>
+      </form>
     </div>
   </div>
 </template>
@@ -21,8 +35,25 @@ import QRCode from 'qrcode'
 export default {
   name: 'app',
   methods: {
+    onCopy() {
+      this.last_notification = "Copied!"
+      this.clearNotification()
+    },
+    onCopyError() {
+      this.last_notification = "Failed to copy :-("
+      this.clearNotification()
+    },
+    clearNotification(opts) {
+      const { after = 1000 } = opts || {};
+      console.log(after)
+      if(this.clear_notification_timeout) {
+        clearTimeout(this.clear_notification_timeout)
+      }
+      this.clear_notification_timeout = setTimeout(() => {
+        this.last_notification = '';
+      }, after)
+    },
     displayQR(url) {
-      console.log('trying to display', url)
       QRCode.toCanvas(this.$refs['qrcanvas'], url)
     },
     async sendMessage() {
@@ -50,6 +81,7 @@ export default {
           break;
         case 'peer_message':
           console.log(message.text)
+          this.messages.push(message.text)
           break;
         default:
           console.log(message)
@@ -58,8 +90,11 @@ export default {
   },
   data() {
     return {
+      clear_notification_timeout: null,
       socket: null,
+      last_notification: null,
       message: '',
+      messages: [],
       my_client_id: null,
       session: {
         linked: false,
@@ -73,7 +108,23 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.message_text {
+  border: dotted;
+  padding: 12px;
+  margin-top: 6px;
+  margin-bottom: 6px;
+  background-color: lightgrey;
+}
+
+.message button {
+  margin-left: 8px;
+}
+
+.message * {
+  display: inline-block;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueClipboard from 'vue-clipboard2'
+
+VueClipboard.config.autoSetContainer = true // add this line
+Vue.use(VueClipboard)
+
 Vue.config.productionTip = false
 
 new Vue({
