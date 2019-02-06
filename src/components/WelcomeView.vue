a9a22778696c629ed3f812d1e7cd6fae0c053918
Tom Klinovsky <tom.klino@gmail.com>
2019-02-06 21:12:01 +0200

    fixed show me how it works

--- src/components/WelcomeView.vue
+++ src/components/WelcomeView.vue
@@ -3,6 +3,14 @@
     v-on:show-video="showVideo()"
     v-on:close="$router.push({ name: 'main' })"
     v-bind:showing="welcome_message_showing" />
+  <v-dialog v-model="video_card" width="1200px" height="680px">
+    <v-card>
+      <iframe width="1120" height="630" src="https://www.youtube-nocookie.com/embed/xkaPmNuDsQ8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <v-card-actions id="buttons">
+        <v-btn @click="video_card=false" flat color="info">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -12,12 +20,13 @@ export default {
   name: 'WelcomeView',
   data() {
     return {
-      welcome_message_showing: true
+      welcome_message_showing: true,
+      video_card: false
     }
   },
   methods: {
     showVideo() {
-      console.log("showVideo: STUB")
+      this.video_card = true;
     }
   },
   components: {
